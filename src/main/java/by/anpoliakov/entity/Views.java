6a2c48251f515840f43782930eaa9978c3ceb3ff
anpoliakov <it.anpoliakov@gmail.com>
2024-01-22 22:30:57 +0300

    Edit descriptions of classes and files

--- src/main/java/by/anpoliakov/entity/Views.java
+++
@@ -1,7 +0,0 @@
-package by.anpoliakov.entity;
-
-/** Класс предназначенный для фильтрации передаваемых пользователю данных
- * используется в аннотации @JsonView Spring приложения*/
-public final class Views {
-    public interface DisplayInfo {}
-}
---
+++ src/main/java/by/anpoliakov/model/Views.java
@@ -0,0 +1,7 @@
+package by.anpoliakov.model;
+
+/** Класс предназначенный для фильтрации передаваемых пользователю данных
+ * используется в аннотации @JsonView Spring приложения, над полями которые необходимо показать пользователю */
+public final class Views {
+    public interface DisplayInfo {}
+}
